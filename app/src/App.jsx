a2053a986947a616e019963860a46f5a7e01ba4e
saksham11kc <96900198+saksham11kc@users.noreply.github.com>
2024-10-14 10:10:40 +0545

    Implement backend search filtering logic

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,129 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Logo from "/DALL·E 2024-10-13 18.19.52 - A vibrant, modern logo for a restaurant called 'Feast Station'. The design should emphasize a station theme, with subtle culinary elements like plates-modified.png";
-const App = () => {
-  return (
-    <Container>
-      <TopContainer>
-        <div className="logo">
-          <img src={Logo} alt="Logo" height="200px" />
-        </div>
-        <div className="search">
-          <input placeholder="Search Food" />
-        </div>
-      </TopContainer>
+import SearchResult from "./components/SearchResults/SearchResult";
+export const BASE_URL = "http://localhost:9000";
 
-      <FilterContainer>
-        <Button>All</Button>
-        <Button>Breakfast</Button>
-        <Button>Lunch</Button>
-        <Button>Dinner</Button>
-      </FilterContainer>
-    </Container>
+const App = () => {
+  const [data, setData] = useState(null);
+  const [filteredData, setFilteredData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedBtn, setSelectedBtn] = useState("all");
+
+  useEffect(() => {
+    const fetchFoodData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(BASE_URL);
+
+        const json = await response.json();
+
+        setData(json);
+        setFilteredData(json);
+        setLoading(false);
+      } catch (error) {
+        setError("Unable to fetch data");
+      }
+    };
+    fetchFoodData();
+  }, []);
+
+  const searchFood = (e) => {
+    const searchValue = e.target.value;
+
+    console.log(searchValue);
+
+    if (searchValue === "") {
+      setFilteredData(null);
+    }
+
+    const filter = data?.filter((food) =>
+      food.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredData(filter);
+  };
+
+  const filterFood = (type) => {
+    if (type === "all") {
+      setFilteredData(data);
+      setSelectedBtn("all");
+      return;
+    }
+
+    const filter = data?.filter((food) =>
+      food.type.toLowerCase().includes(type.toLowerCase())
+    );
+    setFilteredData(filter);
+    setSelectedBtn(type);
+  };
+
+  const filterBtns = [
+    {
+      name: "All",
+      type: "all",
+    },
+    {
+      name: "Breakfast",
+      type: "breakfast",
+    },
+    {
+      name: "Lunch",
+      type: "lunch",
+    },
+    {
+      name: "Dinner",
+      type: "dinner",
+    },
+  ];
+
+  if (error) return <div>{error}</div>;
+  if (loading) return <div>loading.....</div>;
+
+  return (
+    <>
+      <Container>
+        <TopContainer>
+          <div className="logo">
+            <img
+              src={Logo}
+              alt="logo"
+              height="130px"
+              style={{ marginTop: "50px" }}
+            />
+          </div>
+
+          <div className="search">
+            <input onChange={searchFood} placeholder="Search Food" />
+          </div>
+        </TopContainer>
+
+        <FilterContainer>
+          {filterBtns.map((value) => (
+            <Button
+              isSelected={selectedBtn === value.type}
+              key={value.name}
+              onClick={() => filterFood(value.type)}
+            >
+              {value.name}
+            </Button>
+          ))}
+        </FilterContainer>
+      </Container>
+      <SearchResult data={filteredData} />
+    </>
   );
 };
 export default App;
-const Container = styled.div`
+
+export const Container = styled.div`
   max-width: 1200px;
-  margin: auto;
+  margin: 0 auto;
 `;
 const TopContainer = styled.section`
-  min-height: 140px;
+  height: 140px;
   display: flex;
-
   justify-content: space-between;
   padding: 16px;
   align-items: center;
@@ -43,18 +137,34 @@ const TopContainer = styled.section`
       height: 40px;
       font-size: 16px;
       padding: 0 10px;
+      &::placeholder {
+        color: white;
+      }
     }
   }
+
+  @media (0 < width < 600px) {
+    flex-direction: column;
+    height: 120px;
+  }
 `;
+
 const FilterContainer = styled.section`
   display: flex;
   justify-content: center;
   gap: 12px;
+  padding-bottom: 40px;
 `;
-const Button = styled.button`
-  background: #ff4343;
+
+export const Button = styled.button`
+  background: ${({ isSelected }) => (isSelected ? "#f22f2f" : "#ff4343")};
+  outline: 1px solid ${({ isSelected }) => (isSelected ? "white" : "#ff4343")};
   border-radius: 5px;
   padding: 6px 12px;
   border: none;
   color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #f22f2f;
+  }
 `;
